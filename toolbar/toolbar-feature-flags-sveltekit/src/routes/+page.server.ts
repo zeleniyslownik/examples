9db4d20980ea06ac555a2ac8c7630c17c4e10232
zeleniyslownik <165406142+zeleniyslownik@users.noreply.github.com>
2024-07-18 16:57:01 +0300

    Delete toolbar directory

--- toolbar/toolbar-feature-flags-sveltekit/src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { svelteColor } from '$lib/flags'
-
-export async function load() {
-  return {
-    // Requests the feature flag and returns its value
-    svelteColor: await svelteColor(),
-  }
-}
--- toolbar/toolbar-feature-flags-sveltekit/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { vercelToolbar } from '@vercel/toolbar/plugins/vite'
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  plugins: [sveltekit(), vercelToolbar()],
-})
